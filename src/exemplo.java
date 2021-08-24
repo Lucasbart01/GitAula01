bb7749977b10ef8d973294b0da0d831441cdcba2
Lucasbart01 <lucasbartolomeumendes.2020@outlook.com>
2021-08-24 17:39:43 -0300

    Alteração de soma

--- src/exemplo.java
+++ src/exemplo.java
@@ -2,8 +2,8 @@
 public class exemplo {
 	public static void main(String args[]) {
 		
-		int num1=7, num2=10, num3 =3;
-		int total = num1 + num2 + num3;
+		int num1=7, num2=10, num3 =3, num4 = 5;
+		int total = num1 + num2 + num3 + num4;
 		
 		System.out.println("Total:"+ total);
 		System.out.println("Total subtração:"+ (num1 - num2 - num3));
