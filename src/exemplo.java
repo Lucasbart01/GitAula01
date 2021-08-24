6b11ea1e231f9526c97b04ed397e7972639887b6
Lucasbart01 <lucasbartolomeumendes.2020@outlook.com>
2021-08-24 16:57:06 -0300

    Nova operação: subtração

--- src/exemplo.java
+++ src/exemplo.java
@@ -5,7 +5,9 @@ public class exemplo {
 		int num1=7, num2=10, num3 =3;
 		int total = num1 + num2 + num3;
 		
-		System.out.print("valor total"+total);
+		System.out.println("Total:"+ total);
+		System.out.println("Total subtração:"+ (num1 - num2 - num3));
+		
 	}
 
 }
