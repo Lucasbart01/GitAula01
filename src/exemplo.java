14da0d434f018f73ce58377793c887fe9c478466
Lucasbart01 <lucasbartolomeumendes.2020@outlook.com>
2021-08-24 16:40:29 -0300

    alteração de soma

--- src/exemplo.java
+++ src/exemplo.java
@@ -2,7 +2,7 @@
 public class exemplo {
 	public static void main(String args[]) {
 		
-		int num1=2, num2=4;
+		int num1=2, num2=6;
 		int total = num1 + num2;
 		
 		System.out.print("valor total"+total);
