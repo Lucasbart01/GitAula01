5b6ff3f3a4819f0cbd2248b3c8589c486d7f1d62
Lucasbart01 <lucasbartolomeumendes.2020@outlook.com>
2021-08-24 16:54:12 -0300

    Adicionando nova variável

--- src/exemplo.java
+++ src/exemplo.java
@@ -2,8 +2,8 @@
 public class exemplo {
 	public static void main(String args[]) {
 		
-		int num1=2, num2=6;
-		int total = num1 + num2;
+		int num1=7, num2=10, num3 =3;
+		int total = num1 + num2 + num3;
 		
 		System.out.print("valor total"+total);
 	}
